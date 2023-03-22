d5c965cb67dcaece6523b8f46860c1fe27d16816
Felipe Maines <felipemaines123@gmail.com>
2023-03-21 23:52:58 -0300

    Adicionando functions!

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -2,110 +2,195 @@
 {
     internal class Program
     {
+
+        static decimal[] resultadosAntigos = new decimal[100];
+        static int quantidadeDeResultados = 0;
+
+        static void mensagemErro(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensagem);
+            Console.ResetColor();
+        }
+        static string MostrarMenu()
+        {
+            Console.WriteLine("Qual a operacao? (1) Soma / (2) Subtracao / (3) Divisao / (4) Multi: / (5) Ver resultados Antigos! / (6) Para Sair ");
+            string operacao = Console.ReadLine();
+
+            return operacao;
+
+        }
+
+        static bool operacaoInvalida(string opcao)
+        {
+            bool opcaoInvalida =
+                opcao != "1" &&
+                opcao != "2" &&
+                opcao != "3" &&
+                opcao != "4" &&
+                opcao != "5" &&
+                opcao != "6";
+
+            return opcaoInvalida;
+        }
+
+        static decimal pegarValores(string palavras)
+        {
+            Console.WriteLine(palavras);
+            decimal valor = decimal.Parse(Console.ReadLine());
+
+            return valor;
+        }
+
+        static decimal soma (decimal resultado, decimal valor1, decimal valor2, ref int quantidadeDeResultados)
+        {
+            resultado = valor1 + valor2;
+            quantidadeDeResultados++;
+            resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+
+            return resultado;
+        }
+
+        static decimal subtracao (decimal valor1, decimal valor2, decimal resultado, ref int quantidadeDeResultados)
+        {
+            resultado = valor1 - valor2;
+            quantidadeDeResultados++;
+            resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+
+            return resultado;
+        }
+
+        static decimal divisao(decimal valor1, ref decimal valor2, decimal resultado, ref int quantidadeDeResultados)
+
+        {
+            if (valor2 != 0)
+            {
+                resultado = valor1 / valor2;
+                quantidadeDeResultados++;
+                resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+                return resultado;
+            }
+            else {
+
+                do
+                {
+                    mensagemErro("Segundo valor invalido!");
+                    Console.WriteLine("Digite outro valor: ");
+                    valor2 = decimal.Parse(Console.ReadLine());
+                } while (valor2 == 0);
+
+                resultado = valor1 / valor2;
+                quantidadeDeResultados++;
+                return resultado;
+            }
+        }
+
+        static decimal multiplicacao(decimal valor1, decimal valor2, decimal resultado, ref int quantidadeDeResultado)
+        {
+            resultado = valor1 * valor2;
+            quantidadeDeResultado++;
+            resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+            return resultado;
+        }
+
+        static void mostrarResultadosAntigos()
+        {
+            Console.WriteLine("Resultados antigos:");
+            if (quantidadeDeResultados != null) {
+
+                for (int i = 0; i < quantidadeDeResultados; i++)
+                {
+
+                    Console.WriteLine(resultadosAntigos[i]);
+                }
+
+            }
+            else
+            {
+                mensagemErro("Nenhum resultado encontrado! ");
+            }
+            
+        }
+
+        static void mostrarResultado (decimal resultado)
+        {
+            Console.WriteLine($"O resultado eh {Math.Round(resultado, 2)}");
+        }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Calculadora!! ");
 
             decimal resultado = 0;
-            decimal[] resultadosAntigos = new decimal[100];
-            int quantidadeDeResultados = 0;
+           
+            
             string palavra;
             decimal valor1 = 0; 
             decimal valor2 = 0;
+            bool continuar = true;
 
             //Cancelar a operacao caso Escreva "Parar"
-            do
+            while (continuar) 
             {
                 Console.Clear();
 
-                Console.WriteLine("Qual a operacao? (1) Soma / (2) Subtracao / (3) Divisao / (4) Multi: / (5) Ver resultados Antigos! ");
-                decimal operacao = decimal.Parse(Console.ReadLine());
+               string opcao = MostrarMenu();
+               int operacao = int.Parse(opcao);
 
-                //Pede qual operacao q vai ser usada
-                while (operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5)
+                if (operacaoInvalida(opcao))
                 {
-                    Console.WriteLine("Comando invalido!");
-
-                    Console.WriteLine("Qual a operacao? (1) Soma / (2) Subtracao / (3) Divisao / (4) Multi / (5) Ver resultados Antigos! ");
-                    operacao = decimal.Parse(Console.ReadLine());
+                    mensagemErro("Numero Invalido!");
+                    break;
                 }
 
+
+                if (operacao == 6)
+                    break;
                 
+
                 if (operacao != 5)
                 {
-                    Console.Write("Qual o primeiro valor: ");
-                    valor1 = decimal.Parse(Console.ReadLine());
-
-                    Console.Write("Qual o segundo valor: ");
-                    valor2 = decimal.Parse(Console.ReadLine());
+                    valor1 = pegarValores("Qual o primeiro valor: ");
+                    valor2 = pegarValores("Qual o segundo valor: ");
                 }
-                
+
+
 
                 //De acordo com a operacao desejada fez a execulcao
                 switch (operacao)
                 {
                     case 1:
-                        resultado = valor1 + valor2;
-                        quantidadeDeResultados++;
-                        resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+                        resultado = soma(resultado, valor1, valor2, ref quantidadeDeResultados);
+                        mostrarResultado(resultado);
                         break;
 
                     case 2:
-                        resultado = valor1 - valor2;
-                        quantidadeDeResultados++;
-                        resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+                        resultado = subtracao(resultado, valor1, valor2, ref quantidadeDeResultados);
+                        mostrarResultado(resultado);
                         break;
 
                     case 3:
                         //divisao por 0 da infinito (Erro)
-                        while (valor2 == 0)
-                        {
-                            Console.WriteLine("Segundo numero nao pode ser 0, Digite novamente outro numero: ");
-                            Console.Write("Qual o segundo valor: ");
-                            valor2 = decimal.Parse(Console.ReadLine());
-                        }
-                        resultado = valor1 / valor2;
-                        quantidadeDeResultados++;
-                        resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+                        resultado = divisao(valor1, ref valor2, resultado, ref quantidadeDeResultados);
+                        mostrarResultado(resultado);
                         break;
 
                     case 4:
-                        resultado = valor1 * valor2;
-                        quantidadeDeResultados++;
-                        resultadosAntigos[quantidadeDeResultados - 1] = resultado;
+                        resultado = multiplicacao(valor1, valor2, resultado, ref quantidadeDeResultados);
+                        mostrarResultado(resultado);
                         break;
 
                     case 5:
-                        Console.WriteLine("Resultados antigos:");
-                        for (int i = 0; i < quantidadeDeResultados; i++)
-                        {
-                            Console.WriteLine(resultadosAntigos[i]);
-                        }
+                        mostrarResultadosAntigos();
                         break;
-
 
                     default:
-                        Console.WriteLine("Erro");
-                        quantidadeDeResultados++;
-                        resultadosAntigos[quantidadeDeResultados - 1] = 0;
-                        resultado = 0;
+                        mensagemErro("ERRO");
                         break;
                 }
 
-                resultado = Math.Round(resultado, 2);
-
-                if (operacao != 5)
-                {
-                    Console.WriteLine($"O resultado eh {resultado}");
-                }
-                
-                
-
-                //Da a opcao de candelar ou continuar
-                Console.WriteLine("Se desejar parar escreva *Parar* / *Enter* para continuar ");
-                palavra = Console.ReadLine();
-            } while  (palavra.ToUpper() != "PARAR") ; 
+            }
 
         }
     }
